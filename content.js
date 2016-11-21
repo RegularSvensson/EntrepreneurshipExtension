d98865e871bce2b8a14a42ef64b6018363200bf4
RegularSvensson <elias.svensson.1992@gmail.com>
2016-11-21 20:38:28 +0100

    Implement solution to replace n amount of words from webpage

--- content.js
+++ content.js
@@ -1,17 +1,31 @@
+var buzzwords = [{buzzword: "AI", translation: "Computer Overlords"}, 
+{buzzword: "intelligence", translation: "dumbness"}, 
+{buzzword: "machine", translation: "raptor"}]
+
+for (var i = 0; i < buzzwords.length; i++) {
+    console.log(buzzwords[i].buzzword);
+    console.log(buzzwords[i].translation);
+}
+
 var elements = document.getElementsByTagName('*');
 
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
 
     for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+        for (var k = 0; k < buzzwords.length; k++) {
+            var node = element.childNodes[j];
 
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace('AI', 'Computer Overlords');
+            if (node.nodeType === 3) {
+                var text = node.nodeValue;
 
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
+                var regEx = new RegExp(buzzwords[k].buzzword, "gi");
+                var replacedText = text.replace(regEx, buzzwords[k].translation);
+
+                if (replacedText !== text) {
+                    element.replaceChild(document.createTextNode(replacedText), node);
+                }
+
             }
         }
     }
